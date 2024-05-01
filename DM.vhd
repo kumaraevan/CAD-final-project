d46840f0af4751ca4149793bb28d31314fd4578c
kumaraevan <114914008+kumaraevan@users.noreply.github.com>
2024-05-01 20:33:56 +0700

    Update DM.vhd
    Updating DM.vhd to be able to integrate the MSHR.vhd

--- DM.vhd
+++ DM.vhd
@@ -3,11 +3,19 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 
 ENTITY DM IS
-	PORT (
-		I1, I2 : IN STD_ULOGIC_VECTOR(31 DOWNTO 0);
-		O1 : OUT STD_ULOGIC_VECTOR(31 DOWNTO 0);
-		O2 : OUT STD_ULOGIC;
-		C1, C2 : IN STD_ULOGIC);
+    PORT (
+        I1, I2 : IN STD_ULOGIC_VECTOR(31 DOWNTO 0); -- Existing inputs
+        O1 : OUT STD_ULOGIC_VECTOR(31 DOWNTO 0); -- Existing output
+        O2 : OUT STD_ULOGIC; -- Existing output
+        C1, C2 : IN STD_ULOGIC; -- Control signals
+        clk : IN std_logic; -- Clock
+        reset : IN std_logic; -- Reset
+        -- MSHR interface
+        fetch_request : IN STD_ULOGIC_VECTOR(31 DOWNTO 0);
+        fetch_valid : IN STD_ULOGIC;
+        data_out : OUT STD_ULOGIC_VECTOR(31 DOWNTO 0);
+        data_valid : OUT STD_ULOGIC
+    );
 END DM;
 
 ARCHITECTURE DM1 OF DM IS
@@ -47,6 +55,7 @@ ARCHITECTURE DM1 OF DM IS
 
 	SIGNAL ADDRESS, WRITE_DATA, ADDRESS1, WRITE_DATA1, D3, D4, D5, D6, D7, R1 : STD_ULOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
 	SIGNAL MEMWRITE, MEMREAD, MEMWRITE1, MEMREAD1, HIT, L1READY, HIT_D, MEMWRITE_D, MEMREAD_D, L2READY, MUXCTRL, READREADY, WRITEREADY : STD_ULOGIC := '0';
+	SIGNAL data_buffer : STD_ULOGIC_VECTOR(31 DOWNTO 0); -- Buffer to hold fetched data
 BEGIN
 	ADDRESS <= I1; --Address
 	WRITE_DATA <= I2; --Write Data
@@ -84,6 +93,25 @@ BEGIN
 		END IF;
 	END PROCESS;
 
+	mshr_fetch_process : PROCESS (clk. reset)
+	BEGIN
+	    IF reset = '1' THEN
+		data_out <= (others => '0');
+		data_valid <= '0';
+	    ELSIF rising_edge(clk) THEN
+		IF fetch_valid <= '0';
+		    -- Simulate fetching data from memory or lower cache level
+		    -- This is where the actual memory fetch logic is integrated
+		    data_buffer <= fetch_request;
+		    data_valid <= '1';
+		    data_out <= data_buffer;
+		ELSE
+		    data_valid <= '0';
+		END IF;
+	    END IF;
+	END PROCESS;
+
+		    
 	O1 <= R1;
 	O2 <= '1' WHEN (MEMREAD = '0' AND MEMWRITE = '0') ELSE
 		READREADY OR WRITEREADY; --PC&REGENABLE
